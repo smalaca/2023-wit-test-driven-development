b6a7c03f6c747abb83ed32cc61565193464b8b42
smalaca <ab13.krakow@gmail.com>
2023-06-15 13:10:39 +0200

    GREEN - should move forward

--- src/main/java/com/ubs/marsrover/MarsRover.java
+++ src/main/java/com/ubs/marsrover/MarsRover.java
@@ -1,15 +1,17 @@
 package com.ubs.marsrover;
 
 class MarsRover {
-    void land() {
+    private MartianCubit martianCubit;
 
+    void land() {
+        martianCubit = new MartianCubit(0, 0);
     }
 
     MartianCubit position() {
-        return new MartianCubit(0, 0);
+        return martianCubit;
     }
 
     void move(String f) {
-
+        martianCubit = new MartianCubit(1, 0);
     }
 }
